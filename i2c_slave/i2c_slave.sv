05f05b9730fb6b7191cea5eea103fa8422c66ca6
soleiyu <ttahr1404@gmail.com>
2019-08-18 19:38:12 +0900

    slave Upd8

--- i2c_slave/i2c_slave.sv
+++ i2c_slave/i2c_slave.sv
@@ -54,7 +54,7 @@ enc7led cc0(
 initial begin
 	state <= 4'b0;
 	address <=7'h00;
-	myAddress <=7'h42;
+	myAddress <=7'h36;
 	clkcnt <= 4'b0;
 	rw <= 1'b0;
 	ack <= 1'b1;
@@ -65,24 +65,34 @@ always@ (posedge sysclk) begin
 	bscl <= scl;
 	
 	if (!rst) begin
-		state <= 4'hF;
+		state <= 4'h0;
+		address <= 7'b0;
+		clkcnt <= 4'b0;
+		ack <= 1'b1;
 	end
 	else begin
 
-	// negedge sda
-	if (bsda &&	!sda) begin
-		// START
-		if (state != 4'd2) begin
-		if (scl) begin
-			state <= 4'b1;
-
-			address <= 7'b0;
-			clkcnt <= 4'b0;
-
+	// posedge sda
+		if (!bsda && sda) begin
+			if (scl) begin
+				state <= 4'hF;
+			end
 		end
-	end
-	end
-
+	
+	
+	// negedge sda
+		if (bsda &&	!sda) begin
+		// START
+			if (state < 4'd2 || state == 4'hF) begin
+				if (scl) begin
+					state <= 4'b1;
+					address <= 7'b0;
+					clkcnt <= 4'b0;
+//			ack <= 1'b1;
+				end
+			end
+		end
+/*
 	// posedge sda
 	if (!bsda && sda) begin
 		// STOP
@@ -91,43 +101,55 @@ always@ (posedge sysclk) begin
 			state <= 4'hF;
 		end
 	end
+*/
 
-	// posedge scl
-	if (!bscl && scl) begin
-		
-		// COUNT 8
-
-	if (state == 4'd1) begin
-	
-		if (clkcnt == 4'd8) begin
-				clkcnt <= 4'b0;
-				ack <= 1'b1;
-			if (compAddress) begin
-				state <= 4'd2;
+		// negedge scl
+		if (bscl && !scl) begin
+			if (state == 4'd2) begin
+			
+				if (clkcnt == 4'd8) begin
+					ack <= 1'b0;
+				end
+				if (clkcnt == 4'd0) begin
+					ack <= 1'b1;
+					state <= 4'd3;
+				end
+			
 			end
 		end
-		
-		// COUNT 0 - 7
-		
-		else begin
-			clkcnt <= clkcnt + 4'b1;
 
-			// CATCH ADDRESS
-				if (clkcnt < 4'd7) begin
-					address[clkcnt] <= sda;
-				end
-				else if (clkcnt == 4'd7) begin
-					rw <= sda;
-					if (compAddress) begin
-						ack <= 1'b0;
+		// posedge scl
+		if (!bscl && scl) begin
+				// Action For clkcnt
+				if (clkcnt == 4'd8)
+					clkcnt <= 4'b0;
+				else
+					clkcnt <= clkcnt + 1;
+					
+			if (state == 4'd1) begin
+			
+					
+				case (clkcnt)
+					4'd0: address[6] <= sda;
+					4'd1: address[5] <= sda;
+					4'd2: address[4] <= sda;
+					4'd3: address[3] <= sda;
+					4'd4: address[2] <= sda;
+					4'd5: address[1] <= sda;
+					4'd6: address[0] <= sda;
+					4'd7: begin
+						rw <= sda;
+						if (compAddress)
+							state <= 4'd2;
+						end					
+					default: begin
 					end
-				end
+					
+				endcase
+				
 			end
-end
-
 		end
 	end
-
 end
 
 endmodule
