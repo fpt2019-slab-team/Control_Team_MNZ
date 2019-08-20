5377cddb80ffc1281618a557c012473ec4e88404
np418t20m1 <miyata0227karate@gmail.com>
2019-08-20 11:10:33 +0900

    local rule apply

---
+++ KTK_task/modification/src/xcelium.d/run.lnx8664.18.09.d/.timestamp.ts
@@ -0,0 +1,2 @@
+1566199083 /home/users/kotoko/fpt_2019/Control_Team_MNZ/KTK_task/modification/src/count.sv
+1566199171 /home/users/kotoko/fpt_2019/Control_Team_MNZ/KTK_task/modification/src/sim_count.sv
